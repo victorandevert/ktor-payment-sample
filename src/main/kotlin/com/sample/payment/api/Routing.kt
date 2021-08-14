692a5e459f9f0184373afe30242302608e4e0e12
Victor Andevert <victor.andevert@gmail.com>
2021-08-14 19:24:56 +0200

    add error response to payment service

--- src/main/kotlin/com/sample/payment/api/Routing.kt
+++ src/main/kotlin/com/sample/payment/api/Routing.kt
@@ -2,6 +2,7 @@ package com.sample.payment.api
 
 import com.sample.payment.domain.PaymentRepository
 import io.ktor.application.*
+import io.ktor.http.HttpStatusCode.Companion.NotFound
 import io.ktor.response.*
 import io.ktor.routing.*
 
@@ -9,7 +10,11 @@ import io.ktor.routing.*
 fun Route.paymentRouting(paymentRepository: PaymentRepository) {
     route("/payments"){
         get {
-            call.respond(paymentRepository.get())
+            if (paymentRepository.get().isNotEmpty()){
+                call.respond(paymentRepository.get())
+            }else{
+                call.respondText("Payments not found", status = NotFound)
+            }
         }
     }
 }
