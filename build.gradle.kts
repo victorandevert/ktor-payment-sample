aae1bbc0668881fe9b49b224a1ee0d1e1f0edbb6
Victor Andevert <victor.andevert@gmail.com>
2021-08-14 21:07:24 +0200

    add arrow dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,9 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val koin_version: String by project
+val arrow_core: String by project
+val assertj_core: String by project
+val mockito_kotlin: String by project
 
 plugins {
     application
@@ -25,12 +28,13 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.insert-koin:koin-ktor:$koin_version")
+    implementation("io.arrow-kt:arrow-core:$arrow_core")
 
     testImplementation("io.insert-koin:koin-test:$koin_version")
     testImplementation("io.insert-koin:koin-test-junit5:$koin_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
-    testImplementation("org.assertj:assertj-core:3.20.2")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:3.2.0")
+    testImplementation("org.assertj:assertj-core:$assertj_core")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:$mockito_kotlin")
 
 }
 tasks.test {
